474e980f09d566ba393aab7cb481da58083aaeaa
Alfonso Luna <github@lunaembedded.com>
2017-03-20 20:32:47 +0100

    Doxygen style comments

--- source/chip8.h
+++ source/chip8.h
@@ -6,66 +6,73 @@
 #include <stdint.h>
 
 
-/*
-    Clears the memory and copies the charset and a rom to it.
-    rom: A pointer to a chunk of memory containing the rom to be copied.
-    rom_size: The rom size in bytes. The rom can't be larger than 3584 bytes.
-    Returns 0 if the rom was succesfully copied.
-    Returns 1 if the rom was larger than 3584 bytes and hasn't been copied.
-*/
+/**
+ * Clears the memory and copies the charset and a rom to it.
+ *
+ * @param rom A pointer to a chunk of memory containing the rom to be copied.
+ * @param rom_size The rom size in bytes. The rom can't be larger than 3584 bytes.
+ *
+ * @return 0 if the rom was succesfully copied, 1 otherwise.
+ */
 bool chip8ResetMem(uint8_t* rom, int rom_size);
 
 
-/*
-    Resets all registers and buffers.
-*/
+/**
+ * Resets all registers and buffers.
+ */
 void chip8ResetCpu();
 
 
-/*
-    Get the status of a pixel of the screen buffer.
-    Returns 1 if the pixel is set, 0 if the pixel is clear.
-*/
+/**
+ * Get the status of a pixel of the screen buffer.
+ *
+ * @param row The row to be accessed.
+ * @param col The column to be accessed.
+ *
+ * @return 0 if the pixel is clear, 1 if the pixel is set.
+ */
 bool chip8GetPixel(int row, int col);
 
 
-/*
-    Steps the cpu.
-    cycles: The number of instructions to execute.
-    Returns 0 if all instructions were executed succesfully.
-    Returns 1 if an unsupported opcode has been processed.
-*/
+/**
+ * Steps the cpu.
+ *
+ * @param cycles The number of instructions to execute.
+ *
+ * @return 0 if success, 1 if an unsupported opcode has been processed.
+ */
 bool chip8StepCpu(int cycles);
 
 
-/*
-    Decreases the timers.
-    This function needs to be called 60 times per second.
-*/
+/**
+ * Decreases the timers.
+ * This function needs to be called 60 times per second.
+ */
 void chip8UpdateTimers();
 
 
-/*
-    Updates a key status.
-    This function needs to be called when a key has been pressed or released.
-    key: The key pressed or released (from 0x0 to 0xF).
-    status: 1 if the key has been pressed, 0 if the key has been released.
-*/
+/**
+ * Updates a key status.
+ * This function needs to be called when a key has been pressed or released.
+ *
+ * @param key The key pressed or released (from 0x0 to 0xF).
+ * @param status 1 if the key has been pressed, 0 if the key has been released.
+ */
 void chip8UpdateKey(uint8_t key, bool status);
 
 
-/*
-    Get the buzzer status.
-    Returns 1 if the buzzer should beep.
-    Returns 0 if the buzzer should be silent.
-*/
+/**
+ * Get the buzzer status.
+ *
+ * @return 0 if the buzzer should be silent, 1 if should beep.
+ */
 bool chip8GetAudio();
 
 
-/*
-    Get the screen buffer.
-    Returns a pointer to a 128x64 pixels = 8192 bits = 1024 bytes array
-    containing the screen buffer (one bit per pixel).
+/**
+ * Get the screen buffer.
+ *
+ * @return A pointer to the 128x64 pixels = 1024 bytes screen buffer.
 */
 uint8_t* chip8GetScreen();
 
