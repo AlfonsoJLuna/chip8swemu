66cfeeeab68b84ef32de91dd862221301e531d10
Alfonso Luna <github@lunaembedded.com>
2020-12-07 00:16:01 +0100

    Proper timing

--- Platform/GameAndWatch/Source/chip8swemu.c
+++ Platform/GameAndWatch/Source/chip8swemu.c
@@ -14,8 +14,10 @@
 
 void chip8swemu_main_loop()
 {
-    bool Quit;
-    uint16_t GameNumber;
+    bool QuitGame = 0;
+    uint16_t GameNumber = 0;
+    uint32_t CurrentMillis = 0;;
+    uint32_t LastMillis = 0;
 
     // Run the logo ROM for a while
     chip8ResetMem(logo, sizeof(logo));
@@ -27,35 +29,41 @@ void chip8swemu_main_loop()
     // Enter main loop
     while (1)
     {
-        Quit = 0;
+        QuitGame = 0;
         GameNumber = menuSelectGame();
 
         chip8ResetMem(GameList[GameNumber]->GameData, GameList[GameNumber]->GameSize);
         chip8ResetCpu();
-        chip8CompatibilityMode(0);
-        chip8VerticalWrap(1);
+        chip8CompatibilityMode(GameList[GameNumber]->GameEnCompatMode);
+        chip8VerticalWrap(GameList[GameNumber]->GameEnCompatMode);
 
-        while (!Quit)
+        while (!QuitGame)
         {
-            if (GameList[GameNumber]->GameKeyUp    <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyUp,    buttons_get() & B_Up);
-            if (GameList[GameNumber]->GameKeyDown  <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyDown,  buttons_get() & B_Down);
-            if (GameList[GameNumber]->GameKeyLeft  <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyLeft,  buttons_get() & B_Left);
-            if (GameList[GameNumber]->GameKeyRight <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyRight, buttons_get() & B_Right);
-            if (GameList[GameNumber]->GameKeyB     <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyB,     buttons_get() & B_B);
-            if (GameList[GameNumber]->GameKeyA     <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyA,     buttons_get() & B_A);
-            if (GameList[GameNumber]->GameKeyTime  <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyTime,  buttons_get() & B_TIME);
-            if (GameList[GameNumber]->GameKeyPause <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyPause, buttons_get() & B_PAUSE);
+            CurrentMillis = HAL_GetTick();
 
-            chip8UpdateTimers();
-            chip8StepCpu((GameList[GameNumber]->GameFrequency) / 60);
-            displayRefresh(chip8GetScreen());
-            //HAL_Delay(17);
-
-            if (buttons_get() & B_GAME)
+            if ((CurrentMillis - LastMillis) >= 17)
             {
-                Quit = 1;
+                LastMillis = CurrentMillis;
 
-                while (buttons_get() & B_GAME);
+                if (GameList[GameNumber]->GameKeyUp    <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyUp,    buttons_get() & B_Up);
+                if (GameList[GameNumber]->GameKeyDown  <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyDown,  buttons_get() & B_Down);
+                if (GameList[GameNumber]->GameKeyLeft  <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyLeft,  buttons_get() & B_Left);
+                if (GameList[GameNumber]->GameKeyRight <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyRight, buttons_get() & B_Right);
+                if (GameList[GameNumber]->GameKeyB     <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyB,     buttons_get() & B_B);
+                if (GameList[GameNumber]->GameKeyA     <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyA,     buttons_get() & B_A);
+                if (GameList[GameNumber]->GameKeyTime  <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyTime,  buttons_get() & B_TIME);
+                if (GameList[GameNumber]->GameKeyPause <= 0xF) chip8UpdateKey(GameList[GameNumber]->GameKeyPause, buttons_get() & B_PAUSE);
+
+                chip8UpdateTimers();
+                chip8StepCpu((GameList[GameNumber]->GameFrequency) / 60);
+                displayRefresh(chip8GetScreen());
+
+                if (buttons_get() & B_GAME)
+                {
+                    QuitGame = 1;
+
+                    while (buttons_get() & B_GAME);
+                }
             }
         }
     }
